4ee9d4577c9e6351928d3b7db33f28e71d6609f2
haretis <timothee.anne@ens-rennes.fr>
2020-06-22 15:35:42 +0200

    lower max jerk

--- fast_adaptation_embedding/env/evaluate_mpc_directly_on_simulation.py
+++ fast_adaptation_embedding/env/evaluate_mpc_directly_on_simulation.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     # render = True
 
     config = {
-        "n_run": 1,
+        "n_run": 12,
         "episode_length": 500,
 
         "N": '10**(run//3)',
@@ -110,7 +110,7 @@ if __name__ == "__main__":
         'action_dimension': 8,
         "max_vel": 10,
         "max_acc": 100,
-        "max_jerk": 10000,
+        "max_jerk": 1000,
         "dt": 1 / 50,
         "desired_speed": 0.5,
         "reward_weights": [1, 1, 1, 1]
@@ -141,7 +141,7 @@ if __name__ == "__main__":
         json.dump(env_config, fp)
 
     for run in range(config["n_run"]):
-        config['N'] = int(10**3)
+        config['N'] = int(10**(run//3))
 
         env = gym.make("MinitaurTorqueEnv_fastAdapt-v0", **env_config)
         env.metadata['video.frames_per_second'] = 1 / config['dt']
